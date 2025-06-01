f60998dde0c826da1632803e9819919438abb2fc
Cristian <cristian.barquero@isepceu.es>
2025-06-01 20:46:40 +0200

    rectangulo

---
+++ src/cris/Rectangulin.java
@@ -0,0 +1,36 @@
+package cris;
+
+public class Rectangulin {
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getRadio() {
+		return radio;
+	}
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	@Override
+	public String toString() {
+		return "Rectangulin [id=" + id + ", nombre=" + nombre + ", radio=" + radio + ", color=" + color + "]";
+	}
+	private int id;
+	private String nombre;
+	private double radio;
+	private String color;
+}
